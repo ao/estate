5b307738660149ca7ee6a6352b377cdb7de670ee
Andrew Odendaal <andrew.odendaal@travelex.com>
2020-01-13 10:55:11 +0000

    fix tf spinup scripts

--- bootstrapping/aws/resources.tf
+++ bootstrapping/aws/resources.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = var.region
+}
+
 resource "aws_vpc" "estate" {
     cidr_block = "10.0.0.0/16"
 
@@ -7,7 +11,7 @@ resource "aws_vpc" "estate" {
 }
 
 resource "aws_subnet" "estate" {
-    vpc_id = "${aws_vpc.estate.id}"
+    vpc_id = aws_vpc.estate.id
     cidr_block = "10.0.0.0/24"
     availability_zone = "${var.region}a"
 
@@ -19,7 +23,7 @@ resource "aws_subnet" "estate" {
 resource "aws_security_group" "estate" {
     name = "estate_sg"
     description = "Estate"
-    vpc_id = "${aws_vpc.estate.id}"
+    vpc_id = aws_vpc.estate.id
 
     tags {
         Name = "${var.tagName}-SG"
@@ -32,7 +36,7 @@ resource "aws_security_group_rule" "estate_self" {
     to_port = 0
     protocol = "-1"
     self = true
-    security_group_id = "${aws_security_group.estate.id}"
+    security_group_id = aws_security_group.estate.id
 }
 
 resource "aws_security_group_rule" "estate_ssh" {
@@ -41,7 +45,7 @@ resource "aws_security_group_rule" "estate_ssh" {
     to_port = 22
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-    security_group_id = "${aws_security_group.estate.id}"
+    security_group_id = aws_security_group.estate.id
 }
 
 resource "aws_security_group_rule" "estate_outbound" {
@@ -50,7 +54,7 @@ resource "aws_security_group_rule" "estate_outbound" {
     to_port = 0
     protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-    security_group_id = "${aws_security_group.estate.id}"
+    security_group_id = aws_security_group.estate.id
 }
 
 resource "aws_db_subnet_group" "estate" {
@@ -70,10 +74,10 @@ resource "aws_db_instance" "estate" {
     engine_version = "9.5.4"
     instance_class = "db.m3.medium"
     name = "estate"
-    username = "${db_user}"
-    password = "${db_password}"
+    username = db_user
+    password = db_password
     vpc_security_group_ids = ["${aws_security_group.estate.name}"]
-    db_subnet_group_name = ${aws_db_subnet_group.estate.id}
+    db_subnet_group_name = aws_db_subnet_group.estate.id
     skip_final_snapshot = "true"
     backup_retention_period = 0
     copy_tags_to_snapshot = "true"
@@ -105,7 +109,7 @@ resource "aws_elasticache_cluster" "estate" {
     port = 11211
     subnet_group_name = "${aws_elasticache_subnet_group}.estate.name"
     security_group_ids = ["${aws_security_group.estate.name}"]
-    parameter_group_name = "${aws_elasticache_parameter_group.estate.name}"
+    parameter_group_name = aws_elasticache_parameter_group.estate.name
     az_mode = "cross-az"
     maintenance_window = "wed:04:30-wed:05:30"
     tags {
@@ -123,27 +127,27 @@ data "user_data" "estate" {
 }
 
 resource "aws_instance" "estate" {
-    count = "${var.servers}"
-    ami = "${lookup(var.ami, "${var.region}")}"
-    instance_type = "${var.instance_type}"
-    key_name = "${var.key_name}"
+    count = var.servers
+    ami = lookup(var.ami, var.region)
+    instance_type = var.instance_type
+    key_name = var.key_name
     security_groups = ["${aws_security_group.estate.name}"]
     subnet_id = ["${aws_subnet.estate.id}"]
 
     ebs_optimized = true
     disable_api_termination = true
     root_block_device {
-        volume_type = gp2
+        volume_type = "gp2"
         volume_size =  "20"
         delete_on_termination = true
     }
 
     connection {
-        user = "${var.user}"
-        private_key = "${file("${var.key_path}")}"
+        user = var.user
+        private_key = file(var.key_path)
     }
 
-    user_data = "${data.user_data.estate.rendered}"
+    user_data = data.user_data.estate.rendered
 
     tags {
         Name = "${var.tagName}-${count.index}"
